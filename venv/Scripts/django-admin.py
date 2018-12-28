3fc548d86175946d5f5b75d4ff641e9ee6b8432a
Nitin Krishna Joshilkar <nitin@ad.desynova.com>
2018-12-28 13:17:36 +0530

    blog project 28122018new

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Desynova Project\pytest project\django project pytest\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
